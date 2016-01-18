93d5fd0670c92d7e16c74077d5fb4e7c1780e867
HenryGeorgist <willyplehman@gmail.com>
2016-01-17 19:33:08 -0800

    trying to understand how the java.nio.bytebuffer works, not much incremental progress today

--- src/utils/ByteBufferEndian.java
+++ src/utils/ByteBufferEndian.java
@@ -16,8 +16,28 @@ import java.nio.channels.SeekableByteChannel;
  * @author Will_and_Sara
  */
 public class ByteBufferEndian implements java.nio.channels.SeekableByteChannel {
-    private java.nio.ByteBuffer _Buffer;
+    private ByteBuffer _Buffer;
+    //private StringBuffer bla;
+    private boolean SetPosition(int position){
+        try{
+            _Buffer = (ByteBuffer)_Buffer.position(position);
+            
+            return true;
+        }catch(IllegalArgumentException ex){
+            //position is less than mark.
+            //how do i advance the position to the place i want?
+            _Buffer.rewind();
+            if(position>_Buffer.remaining()){
+                //must need to move mark as far forward as i can iteratively?
+            }else{
+                _Buffer.position(position);
+                return true;
+            }
+                    
+            return false;
+        }
 
+    }
     @Override
     public int read(ByteBuffer dst) throws IOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -25,6 +45,9 @@ public class ByteBufferEndian implements java.nio.channels.SeekableByteChannel {
 
     @Override
     public int write(ByteBuffer src) throws IOException {
+        //bla = new StringBuffer();
+        //bla.
+        _Buffer.
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
