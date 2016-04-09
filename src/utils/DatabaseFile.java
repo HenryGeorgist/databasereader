e7ea3d60ba4cd048289b63083feade05299162b9
HenryGeorgist <willyplehman@gmail.com>
2016-04-08 20:50:51 -0700

    minor additions to the file registry idea, next steps are to add them to the abstract database.

--- src/utils/DatabaseFile.java
+++ src/utils/DatabaseFile.java
@@ -13,6 +13,12 @@ public class DatabaseFile {
     private FileModeEnum _Mode;
     private boolean _IsOpen;
     private String _Path;
+    public DatabaseFile(String FilePath, FileModeEnum Mode){
+        _Mode = Mode;
+        _Path = FilePath;
+    }
+    public void Open(){_IsOpen = true;}
+    public void Close(){_IsOpen = false;}
     public FileModeEnum getFileMode(){return _Mode;}
     public boolean IsOpen(){return _IsOpen;}
     public String getFilePath(){return _Path;}
--- src/utils/FileRegistry.java
+++ src/utils/FileRegistry.java
@@ -12,6 +12,24 @@ import java.util.ArrayList;
  */
 public class FileRegistry {
     private static ArrayList<DatabaseFile> _Files;
+    static{
+        _Files = new ArrayList<>();
+    }
+    static void AddFile(String NewFile, FileModeEnum Mode){
+        // check if file is already in list?
+        boolean addfile = true;
+        for(DatabaseFile f: _Files){
+            if(f.getFilePath().equals(NewFile)){
+                if(f.getFileMode() == Mode){
+                    //Same file same mode? what do you do?
+                    addfile = false;
+                }else{
+                    //Same file different mode? what should one do?
+                }
+            }
+        }
+        if(!addfile){_Files.add(new DatabaseFile(NewFile,Mode));}
+    }
     public boolean IsFileOpen(String FilePath){
         return _Files.stream().filter((f) -> (f.getFilePath().equals(FilePath))).anyMatch((f) -> (f.IsOpen()));
     }
